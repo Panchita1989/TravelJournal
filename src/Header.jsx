09e18bfe40a788fcc685223cc787a63a06c7659b
Panchita <franciscalandwehr@hotmail.com>
2025-04-08 13:55:13 -0500

    new Project

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header(){
+   return( 
+    <header>
+        <img FontAwesomeIcon icon="fa-thin fa-earth-americas"/>
+        <h1>This is my travel Journal</h1>
+    </header>
+   )
+}
+
+<a></a>
